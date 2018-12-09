965dbddf0e934802c84c6b37f29ebc9de682580b
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-12-09 12:36:43 +0100

    initial commit 2

--- lib/helpers/db_helpers.dart
+++ lib/helpers/db_helpers.dart
@@ -5,7 +5,7 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:synchronized/synchronized.dart';
 
-final String dbName = "scoreboard.db";
+final String dbName = "board.db";
 final int dbVersion = 1;
 
 class DbHelpers {
@@ -15,10 +15,9 @@ class DbHelpers {
   Future<Database> get db async {
     String databasesPath = await getDatabasesPath();
     String path = join(databasesPath, dbName);
-
     if (_db == null || !_db.isOpen) {
       try {
-        await Directory(path).create(recursive: true);
+        await Directory(databasesPath).create(recursive: true);
       } catch (error) {
         print("Create DB directory error: $error");
       }
@@ -26,11 +25,12 @@ class DbHelpers {
       /// Avoid lock issues on Android
       await _lock.synchronized(() async {
         if (_db == null || !_db.isOpen) {
-          _db = await openDatabase(path, version: dbVersion,
-              onCreate: (Database db, int version) async {
-            await db.execute(DbSql.createMatches);
-            await db.execute(DbSql.createScores);
-            await db.execute(DbSql.createSets);
+          print("******************Opening database**********************");
+          _db = await openDatabase(path, version: dbVersion, onCreate: (Database db, int version) async {
+            await db.execute("${DbSql.createMatches}${DbSql.createScores}${DbSql.createSets}");
+            // await db.execute(DbSql.createMatches);
+            // await db.execute(DbSql.createScores);
+            // await db.execute(DbSql.createSets);
           });
         }
       });
@@ -57,5 +57,10 @@ class DbHelpers {
   Future<int> delete(String table, String id) async {
     Database dbCon = await db;
     return dbCon.delete(table, where: "${DbSql.colId} = ?", whereArgs: [id]);
+  }
+
+  Future<List<Map<String, dynamic>>> query(String table, {bool distinct = false, int limit = -1, String orderBy, String where, List<dynamic> whereArgs}) async {
+    Database dbCon = await db;
+    return dbCon.query(table, columns: [], distinct: distinct, limit: limit, orderBy: orderBy, where: where, whereArgs: whereArgs);
   }
 }
--- lib/test.dart
+++ lib/test.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:locale_scoreboard/helpers/db_helpers.dart';
+import 'package:locale_scoreboard/helpers/db_sql_create.dart';
+import 'package:locale_scoreboard/helpers/system_helpers.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TestWidget extends StatefulWidget {
   static final routeName = "/test";
@@ -16,8 +21,28 @@ class _TestWidgetState extends State<TestWidget> {
           children: <Widget>[
             RaisedButton(
               child: Text("Test"),
-              onPressed: () {
-
+              onPressed: () async {
+                DbHelpers db = DbHelpers();
+                int value = await db.insert(DbSql.tableMatches, {
+                  "id": SystemHelpers.generateUuid(),
+                  "title": "test2",
+                  "createdDate": 0,
+                  "matchDate": 0,
+                  "namePlayer1Team1": "anne",
+                  "namePlayer2Team1": "irene",
+                  "namePlayer1Team2": "bente",
+                  "namePlayer2Team2": "lise",
+                  "active": 0
+                });
+                print(value);
+              },
+            ),
+            RaisedButton(
+              child: Text("Test2"),
+              onPressed: () async {
+                DbHelpers db = DbHelpers();
+                List<Map<String, dynamic>> values = await db.query(DbSql.tableMatches);
+                print(values);
               },
             )
           ],
